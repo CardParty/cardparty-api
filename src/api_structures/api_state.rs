f0f6278f2b767991dac22a4facd0973e96fed5f0
edikiuspy <u96_edumun_waw@technischools.com>
2024-08-17 00:44:39 +0200

    works ig

--- src/api_structures/api_state.rs
+++ src/api_structures/api_state.rs
@@ -1,9 +1,10 @@
+use std::sync::{Arc, Mutex};
 use crate::api_structures::managers::session_manager::SessionManager;
 
 // Globalny stan api
 // dodawac tu tylko najważniejsze żeczy ktore MUSZĄ byc w globalnym stanie
 pub struct ApiState {
-    pub session_manager: SessionManager,
+    pub session_manager: Arc<Mutex<SessionManager>>,
 }
 
 impl ApiState {
