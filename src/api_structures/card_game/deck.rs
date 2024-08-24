7d948b0e2277e288e8e67beccd219728d56e0ace
re-oh <dabrowskilucas2@gmail.com>
2024-08-24 22:51:40 +0200

    added basic websocket_event infastructure into the code

--- src/api_structures/card_game/deck.rs
+++ src/api_structures/card_game/deck.rs
@@ -1,4 +1,50 @@
+use actix::Addr;
+use chrono::prelude::*;
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
+use crate::api_structures::session::{Player, SessionConnection};
 #[derive(Serialize, Deserialize)]
 pub struct Deck {}
+
+pub struct WebsocketEvent {
+    pub meta_data: MetaData,
+    pub data: Data,
+}
+
+pub struct WebsocketEventBuilder {
+    pub meta_data: Option<MetaData>,
+    pub data: Option<Data>,
+}
+
+impl WebsocketEvent {
+    pub fn configure() -> WebsocketEventBuilder {
+        WebsocketEventBuilder {
+            meta_data: None,
+            data: None,
+        }
+    }
+}
+
+pub struct MetaData {
+    pub event_type: EventType,
+    pub timestamp: String,
+    pub event_id: Uuid,
+    pub adressor: Addr<SessionConnection>,
+}
+
+pub enum Data {
+    Deck(Deck),
+    Players(Vec<Player>),
+}
+
+pub enum EventType {
+    Responding,
+    UpdateState,
+    ParseCard,
+    NextPlayer,
+    Close,
+    PlayerJoined,
+    ExecuteCommand,
+    PlayerLeft,
+}
